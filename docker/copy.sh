6cdaafb3913e08b6f0a82825bb83541ae32ac2dc
TsuiMengchao <tsuimengchao@gmail.com>
2024-06-27 14:59:44 +0800

    V1.0.1版-补丁：上传了后端配置文件，使用maven选择activity。修正了docker部署

---
+++ docker/copy.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# 复制项目的文件到对应docker路径，便于一键生成镜像。
+usage() {
+	echo "Usage: sh copy.sh"
+	exit 1
+}
+
+
+# copy sql
+echo "begin copy sql "
+cp ../cxblog-server/sql/cxblog.sql ./cxblog/db
+
+# copy html
+echo "begin copy html "
+cp -r ../cxblog-admin/admin/** ./cxblog/html/admin
+cp -r ../cxblog-web/web/** ./cxblog/html/web
+cp -r ../cxblog-uiapp/unpackage/dist/** ./cxblog/html/h5
+cp -r ../cxblog-doc/doc/** ./cxblog/html/doc
+
+
+# copy jar
+echo "begin copy cxblog-server "
+cp ../cxblog-server/cxblog-main/target/server.jar ./cxblog/server/jar
